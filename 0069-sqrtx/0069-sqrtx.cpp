c0017fab59599f9e613802531ed0c94c44611bf9
Nitigya <nitigyacse@gmail.com>
2026-07-25 17:22:55 +0530

    Time: 0 ms (100%), Space: 8.8 MB (14.81%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0) return 0;
+
+        int low = 1, high = x;
+        int ans = 0;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            // Using division mid <= x / mid to avoid integer overflow of mid * mid
+            if (mid <= x / mid) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
