1780c5d3c7478aa6d98369cf4f03d211169117ab
Nitigya <nitigyacse@gmail.com>
2026-07-27 19:18:48 +0530

    Time: 0 ms (100%), Space: 11.5 MB (79.5%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <utility>
+
+class Solution {
+public:
+    void sortColors(std::vector<int>& nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.size() - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                std::swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else { // nums[mid] == 2
+                std::swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
